65c224140e0990c47afeba26d9ff6d0f1a8f5c6e
Natioterom <natioterom@gmail.com>
2022-10-03 14:53:57 -0300

    avances

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-function numeroTarjeta(){
+import validator from './validator.js';
 
-    const tarjeta=document.getElementById("numeroTc");
-    if(tarjeta.value==("2")){
-        console.log("prueba");
-        alert("prueba");
-    }
-    }
-    
+//Creo variable del boton para usar el addEventListener (id que esta en el boton de html)
+const btn = document.getElementById("validar");
+//Creo variable para utilizar el oninput
+const input = document.getElementById("numeroTc");
+//Creo variable para colocar el logo de la tarjeta
+const logo= document.querySelector("#tarjeta");
+
+ 
+       
+btn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let numCard = document.querySelector("input").value;
+    (validator.isValid(numCard));
+//Enmascarar números
+}
+)
+console.log(validator);
+
+input.addEventListener("keyup",logos);
+function logos(){
+    let numCard = document.querySelector("input").value;
+    let arrNumCard = numCard.split('')
+      if (arrNumCard [0]==4){
+       logo.innerHTML="";
+       const visa= document.createElement("img");
+       visa.src="tarjeta visa.png";
+       logo.appendChild(visa)
+   }
+   else if(arrNumCard [0]==5){
+       logo.innerHTML="";
+       const masterd= document.createElement("img");
+       masterd.src="tarjeta mastercard.png";
+       logo.appendChild(masterd)
+   } 
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
-const validator = {
-  // ...
-};
+const validator= {
+  
+isValid(numCard){
+  //Prevengo el default del formulario
+  //Creo variables
+  let suma1 = 0;
+  let suma2 = [0];
+  let total = 0;
+  let sumaDigitos=0;
+  //Tomo el valor del input (Es un string y lo transformo a array con .split)
+  let arrNumCard = numCard.split('')
+  //if para mostrar el logo de la tarjeta si comienza con 4 es visa si comienza con 5 mastercard
+  //Hacer un ciclo for para multiplicar las posiciones pares usando mod 2.
+  for (let i = 0; i < arrNumCard.length; i++) {
+      let numeroActual = arrNumCard[i];
+      if (i % 2!= 0) {
+          numeroActual = arrNumCard[i] * 2;
+          //Si el resultado es mayor a 9 le resto 9.
+          if (numeroActual > 9) {
+              sumaDigitos = numeroActual - 9;
+              suma1 += sumaDigitos;
+          }
+          else {
+              suma1 += numeroActual;
+             
+          }
+      }
+      else {
+          suma1 += numeroActual;
+          //Suma 1 queda como string y lo transformo a un array.
+          suma2 = suma1.split('');
+      }
+  }
+  //Ciclo for para sumar los numeros del array.
+  for (let i = 0; i < suma2.length; i++) {
+      total+= parseInt(suma2[i]);     
+      }
+    //Si el modulo del total es igual a 10 es valido , de lo contrario invalido.
+  if (total % 10===0){
+          
+    alert("El pago se realizo con éxito")
+    return true;
+      }
+ else{
+   
+        alert ("El pago fue rechazao");
+        return false;               
+ }   
+          
+  } 
+  
 
+}
 export default validator;
