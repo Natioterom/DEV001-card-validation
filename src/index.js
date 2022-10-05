c45b77acaf5dd46f41fda23db8077863eb087eee
Natioterom <natioterom@gmail.com>
2022-10-05 12:48:16 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -6,18 +6,46 @@ const btn = document.getElementById("validar");
 const input = document.getElementById("numeroTc");
 //Creo variable para colocar el logo de la tarjeta
 const logo= document.querySelector("#tarjeta");
+//variables para nombre,fecha y cvv
+const nombre = document.getElementById("nombre");
+const fecha = document.getElementById("fecha");
+const cvv = document.getElementById("cvv");
 
- 
-       
-btn.addEventListener("click", (e)=>{
+btn.addEventListener("click",(e)=>{
     e.preventDefault();
-    let numCard = document.querySelector("input").value;
-    (validator.isValid(numCard));
-//Enmascarar números
-}
-)
-console.log(validator);
-
+    let numCard = document.querySelector("input");
+    if(numCard.value ==""){
+            if(nombre.value ==""){
+        alert("Debes ingresar tú Nombre")
+        }
+    if(fecha.value ==""){
+        alert("Debes ingresar la Fecha de Expiración")
+        } 
+        if(cvv.value ==""){
+        alert("Debes ingresar el Número de CVV")
+             }  
+             alert("Debes ingresar el Número de Tarjeta")
+             return;
+         } 
+                                           
+    else{
+     let valido=validator.isValid(numCard.value);
+     if(valido){
+    alert("El pago fue realizado con éxito");
+     }
+       else{ alert("El pago fue rechazado");
+    }
+       }
+   });
+//Funcion para enmascarar Números
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let numCard = document.getElementById("numeroTc");
+    let numeroImput=validator.maskify(numCard.value);  
+    numCard.value=numeroImput; 
+    });
+    
+//if para mostrar el logo de la tarjeta si comienza con 4 es visa si comienza con 5 mastercard
 input.addEventListener("keyup",logos);
 function logos(){
     let numCard = document.querySelector("input").value;
@@ -36,3 +64,5 @@ function logos(){
        
    } 
 }
+
+
