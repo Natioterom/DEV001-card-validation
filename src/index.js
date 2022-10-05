a4ee0bd6659959a68c0212183c39e2a270bc96f8
Natioterom <natioterom@gmail.com>
2022-10-05 16:59:59 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -5,64 +5,71 @@ const btn = document.getElementById("validar");
 //Creo variable para utilizar el oninput
 const input = document.getElementById("numeroTc");
 //Creo variable para colocar el logo de la tarjeta
-const logo= document.querySelector("#tarjeta");
+const logo = document.querySelector("#tarjeta");
 //variables para nombre,fecha y cvv
 const nombre = document.getElementById("nombre");
 const fecha = document.getElementById("fecha");
 const cvv = document.getElementById("cvv");
 
-btn.addEventListener("click",(e)=>{
+
+
+btn.addEventListener("click", (e) => {
     e.preventDefault();
     let numCard = document.querySelector("input");
-        if(nombre.value ==""){
+    if (nombre.value == "") {
         alert("Debes ingresar tú Nombre")
-        }
-    if(fecha.value ==""){
-        alert("Debes ingresar la Fecha de Expiración")
-        } 
-        if(cvv.value ==""){
-        alert("Debes ingresar el Número de CVV")
-             }  
-             if(numCard.value ==""){
-                alert("Debes ingresar el Número de Tarjeta")
-                return;
-                }    
-    else{
-     let valido=validator.isValid(numCard.value);
-     if(valido){
-    alert("El pago fue realizado con éxito");
-     }
-       else{ alert("El pago fue rechazado");
+        document.getElementById('nombre').style.borderColor = "red"
     }
-       }
-   });
+    if (fecha.value == "") {
+        alert("Debes ingresar la Fecha de Expiración")
+        document.getElementById('fecha').style.borderColor = "red"
+    }
+    if (cvv.value == "") {
+        alert("Debes ingresar el Número de CVV")
+        document.getElementById('cvv').style.borderColor = "red"
+    }
+    if (numCard.value == "") {
+        alert("Debes ingresar el Número de Tarjeta")
+        document.querySelector("input").style.borderColor = "red"
+        return;
+    }
+    else {
+        let valido = validator.isValid(numCard.value);
+        if (valido) {
+            alert("El pago fue realizado con éxito");
+        }
+        else {
+            alert("El pago fue rechazado");
+        }
+    }
+});
 
 //Funcion para enmascarar Números
-btn.addEventListener("click",(e)=>{
+btn.addEventListener("click", (e) => {
     e.preventDefault();
     let numCard = document.getElementById("numeroTc");
-    let numeroImput=validator.maskify(numCard.value);  
-    numCard.value=numeroImput; 
-    });
-    
+    let numeroImput = validator.maskify(numCard.value);
+    numCard.value = numeroImput;
+});
+
 //if para mostrar el logo de la tarjeta si comienza con 4 es visa si comienza con 5 mastercard
-input.addEventListener("keyup",logos);
-function logos(){
+input.addEventListener("keyup", logos);
+function logos() {
     let numCard = document.querySelector("input").value;
     let arrNumCard = numCard.split('')
-      if (arrNumCard [0]==4){
-       logo.innerHTML="";
-       const visa= document.createElement("img");
-       visa.src="tarjeta visa.png";
-       logo.appendChild(visa)
-   }
-   else if(arrNumCard [0]==5){
-       logo.innerHTML="";
-       const masterd= document.createElement("img");
-       masterd.src="tarjeta mastercard.png";
-       logo.appendChild(masterd)
-       
-   } 
+    if (arrNumCard[0] == 4) {
+        logo.innerHTML = "";
+        const visa = document.createElement("img");
+        visa.src = "tarjeta visa.png";
+        logo.appendChild(visa)
+    }
+    else if (arrNumCard[0] == 5) {
+        logo.innerHTML = "";
+        const masterd = document.createElement("img");
+        masterd.src = "tarjeta mastercard.png";
+        logo.appendChild(masterd)
+
+    }
 }
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,56 +1,56 @@
-const validator= {
-  
-isValid(numCard){
-  //Prevengo el default del formulario
-  //Creo variables
-  let suma1 = 0;
-  let suma2 = [0];
-  let total = 0;
-  let sumaDigitos=0; 
+const validator = {
+
+  isValid(numCard) {
+    //Prevengo el default del formulario
+    //Creo variables
+    let suma1 = 0;
+    let suma2 = [0];
+    let total = 0;
+    let sumaDigitos = 0;
     //Tomo el valor del input (Es un string y lo transformo a array con .split)
-  let arrNumCard = numCard.split('')
-  //Hacer un ciclo for para multiplicar las posiciones pares usando mod 2.
-  for (let i = 0; i < arrNumCard.length; i++) {
+    let arrNumCard = numCard.split('')
+    //Hacer un ciclo for para multiplicar las posiciones pares usando mod 2.
+    for (let i = 0; i < arrNumCard.length; i++) {
       let numeroActual = arrNumCard[i];
-      if (i % 2!= 0) {
-          numeroActual = arrNumCard[i] * 2;
-          //Si el resultado es mayor a 9 le resto 9.
-          if (numeroActual > 9) {
-              sumaDigitos = numeroActual - 9;
-              suma1 += sumaDigitos;
-          }
-          else {
-              suma1 += numeroActual;
-             
-          }
-      }
-      else {
+      if (i % 2 != 0) {
+        numeroActual = arrNumCard[i] * 2;
+        //Si el resultado es mayor a 9 le resto 9.
+        if (numeroActual > 9) {
+          sumaDigitos = numeroActual - 9;
+          suma1 += sumaDigitos;
+        }
+        else {
           suma1 += numeroActual;
-          //Suma 1 queda como string y lo transformo a un array.
-          suma2 = suma1.split('');
+
         }
       }
-  //Ciclo for para sumar los numeros del array.
-  for (let i = 0; i < suma2.length; i++) {
-      total+= parseInt(suma2[i]);     
+      else {
+        suma1 += numeroActual;
+        //Suma 1 queda como string y lo transformo a un array.
+        suma2 = suma1.split('');
       }
+    }
+    //Ciclo for para sumar los numeros del array.
+    for (let i = 0; i < suma2.length; i++) {
+      total += parseInt(suma2[i]);
+    }
     //Si el modulo del total es igual a 10 es valido , de lo contrario invalido.
-  if (total % 10===0){
-    return true;
-      }
- else{
-  return false;                
- }   
- },
+    if (total % 10 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
 
-maskify(numCard){
-  return numCard.slice(0,-4).replace(/./g, "#") + numCard.slice(-4);
-   //let arrNumCard= numCard.split('');
-   //let primerosNumeros= arrNumCard.slice(0,-4).replace('#');
-   //let ultimoNumeros= arrNumCard.slice(-4); 
-   //let numerosMask= primerosNumeros + ultimoNumeros.join("");
-   //console.log(numerosMask);
-          // return numerosMask;
-}
+  maskify(numCard) {
+    return numCard.slice(0, -4).replace(/./g, "#") + numCard.slice(-4);
+    //let arrNumCard= numCard.split('');
+    //let primerosNumeros= arrNumCard.slice(0,-4).replace('#');
+    //let ultimoNumeros= arrNumCard.slice(-4); 
+    //let numerosMask= primerosNumeros + ultimoNumeros.join("");
+    //console.log(numerosMask);
+    // return numerosMask;
+  }
 }
 export default validator;
